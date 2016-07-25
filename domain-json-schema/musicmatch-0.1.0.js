f225c9beacdcb62ac94ee50be08b187a95c6285a
Derek Kobylarz <darek.kobylarz@gmail.com>
2016-07-25 22:54:26 +0200

    musicmatch domain

---
+++ domain-json-schema/musicmatch-0.1.0.js
@@ -0,0 +1,130 @@
+if(typeof(VITAL_JSON_SCHEMAS) == 'undefined') {
+ throw ("No VITAL_JSON_SCHEMAS list defined - vital-core domain unavailable");
+}
+
+var musicmatch_0_1_0_schema = {
+  "domainURI" : "http://vitalai.com/ontology/musicmatch",
+  "name" : "musicmatch-0.1.0",
+  "vitalsignsVersion" : "0.2.304",
+  "parents" : [ "http://vital.ai/ontology/vital", "http://vital.ai/ontology/vital-recommendation" ],
+  "schemas" : [ {
+    "id" : "http://vitalai.com/ontology/musicmatch#MusicArtist",
+    "parent" : "http://vital.ai/ontology/vital-core#VITAL_Node",
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "http://vitalai.com/ontology/musicmatch#MusicArtist",
+    "description" : "Auto-generated schema for owl class: http://vitalai.com/ontology/musicmatch#MusicArtist",
+    "type" : "object",
+    "properties" : {
+      "URI" : {
+        "type" : "string"
+      },
+      "type" : {
+        "enum" : [ "http://vitalai.com/ontology/musicmatch#MusicArtist" ]
+      },
+      "types" : {
+        "type" : "array",
+        "minItems" : 1,
+        "items" : {
+          "type" : "string"
+        },
+        "uniqueItems" : true
+      },
+      "http://vital.ai/ontology/vital#hasCertainty" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasDegree" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasPageRank" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasName" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasOntologyIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasProvenance" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasTimestamp" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasUpdateTime" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasVersionIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#isActive" : {
+        "type" : "boolean"
+      },
+      "http://vital.ai/ontology/vital-recommendation#isArchetype" : {
+        "type" : "boolean"
+      }
+    },
+    "required" : [ "URI", "type" ],
+    "additionalProperties" : false
+  }, {
+    "id" : "http://vitalai.com/ontology/musicmatch#MusicListener",
+    "parent" : "http://vital.ai/ontology/vital-core#VITAL_Node",
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "http://vitalai.com/ontology/musicmatch#MusicListener",
+    "description" : "Auto-generated schema for owl class: http://vitalai.com/ontology/musicmatch#MusicListener",
+    "type" : "object",
+    "properties" : {
+      "URI" : {
+        "type" : "string"
+      },
+      "type" : {
+        "enum" : [ "http://vitalai.com/ontology/musicmatch#MusicListener" ]
+      },
+      "types" : {
+        "type" : "array",
+        "minItems" : 1,
+        "items" : {
+          "type" : "string"
+        },
+        "uniqueItems" : true
+      },
+      "http://vital.ai/ontology/vital#hasCertainty" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasDegree" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasPageRank" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasName" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasOntologyIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasProvenance" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasTimestamp" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasUpdateTime" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasVersionIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#isActive" : {
+        "type" : "boolean"
+      },
+      "http://vital.ai/ontology/vital-recommendation#isArchetype" : {
+        "type" : "boolean"
+      }
+    },
+    "required" : [ "URI", "type" ],
+    "additionalProperties" : false
+  } ],
+  "properties" : [ ]
+};
+
+ VITAL_JSON_SCHEMAS.push(musicmatch_0_1_0_schema);
